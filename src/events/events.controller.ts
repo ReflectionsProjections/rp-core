9d1e7bdbd9a15bd37c4102a11cc2ba334e833fc8
Aniket Gargya <gargya.aniket@gmail.com>
2023-04-23 16:00:43 -0500

    add events patch method
    Create a `PATCH` route that receives the id via a url param, and the actual data in the body, added a `MongoIdPipe` to ensure the url param string can be cast to an ObjectId

--- src/events/events.controller.ts
+++ src/events/events.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { EventsService } from './events.service';
 import { CreateEventDto } from './dto/create-event.dto';
 import { UpdateEventDto } from './dto/update-event.dto';
+import { MongoIdPipe } from './pipes/mongo-id.pipe';
 
 @Controller('events')
 export class EventsController {
@@ -23,8 +24,8 @@ export class EventsController {
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateEventDto: UpdateEventDto) {
-    return this.eventsService.update(+id, updateEventDto);
+  update(@Param('id', MongoIdPipe) id: string, @Body() updateEventDto: UpdateEventDto) {
+    return this.eventsService.update(id, updateEventDto);
   }
 
   @Delete(':id')
