86b20b6c184c67d55daa74b1d5f78688e6d0530e
Aniket Gargya <gargya.aniket@gmail.com>
2023-05-27 00:08:37 -0500

    :art: use nest generated pipe format for mongo-id pipe

--- src/events/events.controller.ts
+++ src/events/events.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { EventsService } from './events.service';
 import { CreateEventDto } from './dto/create-event.dto';
 import { UpdateEventDto } from './dto/update-event.dto';
-import { MongoIdPipe } from './pipes/mongo-id.pipe';
+import { MongoIdPipe } from '../mongo-id/mongo-id.pipe';
 
 @Controller('events')
 export class EventsController {
@@ -25,6 +25,7 @@ export class EventsController {
 
   @Patch(':id')
   update(@Param('id', MongoIdPipe) id: string, @Body() updateEventDto: UpdateEventDto) {
+    console.log(updateEventDto);
     return this.eventsService.update(id, updateEventDto);
   }
 
