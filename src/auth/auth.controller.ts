8e034572331ea2325638cb0775d036c8d5326e3e
Soham Kulkarni <sohamkrox@gmail.com>
2023-06-02 21:01:44 -0700

    Implement flow for generating & hasing passcodes

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,10 +1,15 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { GeneratePasscodeDto } from './dto/generate-passcode.dto';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
   @Post('/generate')
-  generateVerificationPasscode() {
+  generateVerificationPasscode(@Body() body: GeneratePasscodeDto) {
     // TODO: generateVerificationPasscode
+    this.authService.generateVerificationPasscode(body.email);
   }
 
   @Post('/verify')
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,44 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Verification } from './verifications.schema';
+import { Model } from 'mongoose';
+import crypto from 'crypto';
+import bcrypt from 'bcrypt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  SALT_ROUNDS = 10;
+
+  constructor(
+    @InjectModel(Verification.name)
+    private verificationModel: Model<Verification>,
+  ) {}
+
+  async generateVerificationPasscode(email: string) {
+    // At this point there may or may not be an attendee object.
+    // We simply care about sending an email with the code.
+
+    const passcode = crypto.randomInt(100000, 999999).toString();
+    console.log('passcode', passcode);
+
+    bcrypt.hash(passcode, this.SALT_ROUNDS, async (err, hash) => {
+      if (err) {
+        console.error('BCrypt hash failed');
+      }
+
+      const result = this.verificationModel.create({
+        email,
+        passcodeHash: hash,
+        expiresAt: Date.now().toLocaleString(),
+      });
+
+      console.log('result from insert', result);
+    });
+  }
+
+  verifyPasscode(email: string, passcode: string) {}
+
+  regenerateVerificationPasscode(email: string) {}
+
+  getLoggedInUser() {}
+}
