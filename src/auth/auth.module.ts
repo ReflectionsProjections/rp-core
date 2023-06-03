7d97615848563207f2bcf0cc592311ba5233e4fb
Soham Kulkarni <sohamkrox@gmail.com>
2023-06-02 20:40:42 -0700

    Add Verification model to auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { Verification, VerificationSchema } from './verifications.schema';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: Verification.name, schema: VerificationSchema },
+    ]),
+  ],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService],
+  exports: [
+    MongooseModule.forFeature([
+      { name: Verification.name, schema: VerificationSchema },
+    ]),
+  ],
 })
 export class AuthModule {}
