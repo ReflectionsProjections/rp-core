522339e3192e42a44def652093741ff128490981
Soham Kulkarni <sohamkrox@gmail.com>
2023-06-02 21:19:19 -0700

    Fix runtime errors: ES6 imports & mongoose ref syntax

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,8 +2,8 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Verification } from './verifications.schema';
 import { Model } from 'mongoose';
-import crypto from 'crypto';
-import bcrypt from 'bcrypt';
+const crypto = require('crypto');
+const bcrypt = require('bcrypt');
 
 @Injectable()
 export class AuthService {
@@ -18,21 +18,27 @@ export class AuthService {
     // At this point there may or may not be an attendee object.
     // We simply care about sending an email with the code.
 
+    // TODO Reject if code has been generated within the last 10 seconds
+
+    // Create a secure six digit passcode
     const passcode = crypto.randomInt(100000, 999999).toString();
-    console.log('passcode', passcode);
 
     bcrypt.hash(passcode, this.SALT_ROUNDS, async (err, hash) => {
       if (err) {
-        console.error('BCrypt hash failed');
+        console.error('BCrypt hash failed!');
       }
 
-      const result = this.verificationModel.create({
-        email,
-        passcodeHash: hash,
-        expiresAt: Date.now().toLocaleString(),
-      });
-
-      console.log('result from insert', result);
+      // TODO: Update expiredAt
+      this.verificationModel
+        .create({
+          email,
+          passcodeHash: hash,
+          expiresAt: new Date().toISOString(),
+          createdAt: new Date().toISOString(),
+        })
+        .then((result) => {
+          console.log('result', result);
+        });
     });
   }
 
