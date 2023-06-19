6ab53cd9b6acb3fe78352fb9180840fc8d6e9ea9
Soham Kulkarni <sohamkrox@gmail.com>
2023-06-18 18:22:27 -0700

    Configure CORS to work with credentials

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(helmet());
   app.use(cookieParser());
-  app.enableCors();
+  app.enableCors({
+    credentials: true,
+    origin: 'http://localhost:5173',
+    allowedHeaders: ['Content-Type', 'Set-Cookie'],
+  });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
