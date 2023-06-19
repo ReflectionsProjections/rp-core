70ce077996aead7b67be59ea665bda218700a43a
samyak <samyak257@gmail.com>
2023-06-19 15:47:06 -0700

    🧹 clean up and fix duplication bug

--- src/events/events.service.ts
+++ src/events/events.service.ts
@@ -33,42 +33,16 @@ export class EventsService {
   }
 
   async schedule() {
-    // const all_events = this.eventModel.find().cursor();
-
-
-    // const aggregated_data =  this.eventModel.aggregate( 
-    //   [
-    //     {
-    //       $project:
-    //         {
-    //           "dayOfWeek": { $dayOfWeek: "$_id" }
-    //         }
-    //     }
-    //   ]
-    // )
-
-    // for (let doc = all_events.next(); doc != null; doc = all_events.next()) {
-    //   console.log(doc);
-    // }
-
-    // return {"all" : all_events_list};
     
     try {
       const all_events = await this.eventModel.find().cursor();
-
-      // let monday = [];
-      // let tuesday = [];
-      // let wednesday = [];
-      // let thursday = [];
-      // let friday = [];
-      // let saturday = [];
-      // let sunday = [];
 
       let twoDArray = [[], [], [], [], [], [], [], []]
 
       for await (const doc of all_events) {
         let num = doc.start_time.getDay();
-        twoDArray[num].push(doc);
+        if(!twoDArray[num].includes(doc))
+          twoDArray[num].push(doc);
       }
 
       const res = {
