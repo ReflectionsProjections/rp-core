2d7a1b3eb25200a6cbf33ee173979d92e17a1acd
Rohan Nunugonda <45941834+rohannunu@users.noreply.github.com>
2023-06-19 21:42:00 -0500

    Merge branch 'auth' into reg-endpoint-merge-with-auth
    Commented out useGlobalPipes from main.ts, as it was causing issues with existing registration field input validation

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
+import * as cookieParser from 'cookie-parser';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(helmet());
+  app.use(cookieParser());
   app.enableCors();
+  //app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   await app.listen(3000);
 }
 bootstrap();
