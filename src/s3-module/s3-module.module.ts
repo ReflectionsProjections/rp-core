f13f4f6b17837e611263bd1aaca6a14bf9f17322
Shreya Deshpande <shreyad4@illinois.edu>
2023-06-24 14:07:34 -0500

    added methods for getting and putting objects in s3 bucket

--- src/s3-module/s3-module.module.ts
+++ src/s3-module/s3-module.module.ts
@@ -1,4 +1,74 @@
 import { Module } from '@nestjs/common';
+import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
+import { GetObjectCommand } from "@aws-sdk/client-s3";
+import * as AWS from "aws-sdk";
+
+const client = new S3Client({})
+
 
 @Module({})
-export class S3ModuleModule {}
+export class S3ModuleModule {
+  AWS_S3_BUCKET = process.env.AWS_S3_BUCKET;
+  s3 = new AWS.S3
+  ({
+      accessKeyId: process.env.AWS_S3_ACCESS_KEY,
+      secretAccessKey: process.env.AWS_S3_KEY_SECRET,
+  });
+
+  async uploadFile(file)
+  {
+      const { originalname } = file;
+
+      await this.s3_upload(file.buffer, this.AWS_S3_BUCKET, originalname, file.mimetype);
+  }
+
+  async s3_upload(file, bucket, name, mimetype)
+  {
+      const params = 
+      {
+          Bucket: bucket,
+          Key: String(name),
+          Body: file,
+          ACL: "public-read",
+          ContentType: mimetype,
+          ContentDisposition:"inline",
+          CreateBucketConfiguration: 
+          {
+              LocationConstraint: "ap-south-1"
+          }
+      };
+
+      console.log(params);
+
+      try
+      {
+          let s3Response = await this.s3.upload(params).promise();
+
+          console.log(s3Response);
+      }
+      catch (e)
+      {
+          console.log(e);
+      }
+  }
+
+  async s3_download(file, bucket, name, mimetype) {
+    const command = new GetObjectCommand({
+      Bucket: bucket,
+      Key: file
+    });
+  
+    try {
+      const response = await client.send(command);
+      // The Body object also has 'transformToByteArray' and 'transformToWebStream' methods.
+      const str = await response.Body.transformToString();
+      console.log(str);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  
+      
+}
+  
