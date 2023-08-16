6614840fc298052904642eda2a531be4e9f8ef0d
Soham Kulkarni <sohamkrox@gmail.com>
2023-08-16 14:45:45 -0700

    Add endpoints for QRP with unit tests

--- src/events/dto/register-attendee.dto.ts
+++ src/events/dto/register-attendee.dto.ts
@@ -1,6 +1,15 @@
-import { IsString, IsMongoId } from 'class-validator';
+import { IsString, IsMongoId, IsEmail } from 'class-validator';
 
 export class RegisterAttendeeDto {
   @IsMongoId()
   id: string;
+}
+
+export class RegisterAttendeeEmailDto {
+  @IsEmail()
+  email: string;
+}
+
+export class RegisterAttendeeQRDto {
+  token: string;
 }
