4417b4b412698d798d9e6776ea8996126faa4aff
Soham Kulkarni <sohamkrox@gmail.com>
2023-08-17 22:53:37 -0700

    Add endpoints for QRP with unit tests (#36)
    * Add endpoints for QRP with unit tests
    
    * Add missing decorator to RegisterAttendeeQRDto
    
    * Cleanup code & fix magic string

--- src/events/dto/register-attendee.dto.ts
+++ src/events/dto/register-attendee.dto.ts
@@ -1,6 +1,16 @@
-import { IsString, IsMongoId } from 'class-validator';
+import { IsString, IsMongoId, IsEmail } from 'class-validator';
 
 export class RegisterAttendeeDto {
   @IsMongoId()
   id: string;
+}
+
+export class RegisterAttendeeEmailDto {
+  @IsEmail()
+  email: string;
+}
+
+export class RegisterAttendeeQRDto {
+  @IsString()
+  token: string;
 }
