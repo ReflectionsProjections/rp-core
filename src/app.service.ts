bf5501601a79e33b39b7a37238916dee4133cae8
Soham Kulkarni <sohamkrox@gmail.com>
2023-08-22 17:41:44 -0500

    Bump minor version

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'v1.0.0';
+  getVersion(): string {
+    return 'v1.0.1';
   }
 }
