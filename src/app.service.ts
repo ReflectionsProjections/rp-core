796b858a14e0e99129cf1af4eb174d53cd01116f
Soham Kulkarni <sohamkrox@gmail.com>
2023-08-25 00:16:51 -0500

    Bump minor version & add logs

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,13 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
+  /**
+   * --- Version Log ---
+   * v1.0.0 -> Release Day API
+   * v1.0.* -> Minor fixes not affecting API outputs
+   * v1.1.0 -> Endpoints supporting preferences page
+   */
   getVersion(): string {
-    return 'v1.0.2';
+    return 'v1.1.0';
   }
 }
